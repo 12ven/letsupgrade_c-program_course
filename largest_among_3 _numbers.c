4c8eeb57c7f0e32de3331e4a984b7c37066e62ad
Vengal rao <103012129+12ven@users.noreply.github.com>
2023-04-06 20:50:55 +0530

    Day3_assignment

---
+++ largest_among_3 _numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    int a,b,c;
+    printf("Enter the  first number :");
+    scanf("%d",&a);
+    printf("Enter the second number :");
+    scanf("%d",&b);
+    printf("Enter the third number :");
+    scanf("%d",&c);
+    
+    if (a>=b && a>=c)
+        printf("%d is the largest number.", a);
+    else if (b>=c)
+        printf("%d is the largest number.", b);
+    else
+        printf("%d is the largest number.", c);
+return 0;
+}
